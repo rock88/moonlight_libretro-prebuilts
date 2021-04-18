06a38fc63aba654d6cfa0a878ae922eaf4b8a6c9
Andrey Konoplyankin <rock88a@gmail.com>
2021-04-18 17:34:32 +0300

    Add mac os prebuilded libs

---
+++ mac/build_ffmpeg.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+INSTALL_DIR=`pwd`/ffmpeg
+
+rm -fr ffmpeg_tmp
+git clone https://github.com/FFmpeg/FFmpeg.git ffmpeg_tmp
+cd ffmpeg_tmp
+
+./configure --disable-everything --enable-decoder=h264 --enable-decoder=hevc --disable-programs --disable-ffmpeg \
+  --disable-ffplay --disable-ffprobe --disable-doc --disable-avdevice \
+  --disable-avformat --disable-swresample --disable-swscale --disable-postproc --disable-avfilter \
+  --disable-iconv --disable-lzma --extra-cflags="-mmacosx-version-min=10.12" --extra-ldflags="-mmacosx-version-min=10.12" --prefix=$INSTALL_DIR
+
+make -j
+make install
+
+cd ..
+rm -fr ffmpeg_tmp
