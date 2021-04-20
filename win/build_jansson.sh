04686fce5e4c6322b7dee902e03b91d72c66331f
Andrey Konoplyankin <rock88a@gmail.com>
2021-04-20 23:17:01 +0300

    Add win jansson

--- win/build_jansson.sh
+++ win/build_jansson.sh
@@ -6,11 +6,8 @@ rm -fr jansson_tmp
 git clone https://github.com/akheron/jansson.git jansson_tmp
 cd jansson_tmp
 
-export CFLAGS="-mmacosx-version-min=10.12"
-export ASFLAGS="-mmacosx-version-min=10.12"
-
 autoreconf -fi
-./configure --enable-static --disable-shared --prefix=$INSTALL_DIR
+./configure --host=x86_64 --enable-static --disable-shared --prefix=$INSTALL_DIR
 
 make -j
 make install
